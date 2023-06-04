9ce129acb1ec1fd8b0097b54e18ac6e8c1ca31e3
SREEPRIYA KURUP <53760989+Sreepriyakurup@users.noreply.github.com>
2023-06-04 07:53:19 +0530

    Add files via upload

---
+++ Python_Assignment7.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Q.1. Create two int type variables, apply addition, subtraction, division and multiplications
+# and store the results in variables. Then print the data in the following format by calling the
+# variables:
+
+# a= int(input("Enter first number: "))
+# b= int(input("Enter first number: "))
+# 
+# print("First variable is ", a , '& second variable is', b)
+# print('Addition : ', a,'+',b ,'= ', a+b)
+# print('Subtraction : ', a,'-',b ,'= ', a-b)
+# print('Multiplication : ', a,'*',b ,'= ', a*b)
+# print('Division : ', a,'/',b ,'= ', a/b)
+# 
+
+# Enter first number: 2
+# Enter first number: 3
+# First variable is  2 & second variable is 3
+# Addition :  2 + 3 =  5
+# Subtraction :  2 - 3 =  -1
+# Multiplication :  2 * 3 =  6
+# Division :  2 / 3 =  0.6666666666666666
+
+# Q.2. What is the difference between the following operators:
+#     
+# (i) ‘/’ & ‘//’
+# '/' represents division
+# '//' represents floor division
+# 
+# (ii) ‘**’ & ‘^’
+# '**' represents exponent operator
+# '^' represents EXOR operator
+
+# Q.3. List the logical operators.
+# 
+# Logical operators are and, or, not.
+
+# Q.4. Explain right shift operator and left shift operator with examples.
+# 
+# Right Shift operator: Shifts the bits of the number to the right by the number of bits specified. Each shift to the right corresponds to dividing the number by 2.
+# 
+
+# a=16
+# result = a >> 3
+# print(result)
+
+# 2
+
+# Left Shift Operator: Shifts the bits of the number to the left by the number of bits specified. Each shift to the left corresponds to multiplying the number by 2.
+
+# a=16
+# result = a << 3
+# print(result)
+
+# 128
+
+# Q.5. Create a list containing int type data of length 15. Then write a code to check if 10 is
+# present in the list or not.
+
+# l=[1,2,3,4,5,6,7,8,10,11,12,13,14,15]
+# print(10 in l)               
+#     
+
+# True
